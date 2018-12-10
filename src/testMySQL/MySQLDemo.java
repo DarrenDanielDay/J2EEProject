9c14b2200121ab72b127a13c22f0a04b4cfb15d8
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2018-12-10 21:22:15 +0800

    New project created.

---
+++ src/testMySQL/MySQLDemo.java
@@ -0,0 +1,29 @@
+package testMySQL;
+
+import java.sql.*;
+
+public class MySQLDemo {
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			System.out.println("Success loading Mysql Driver!");
+		} catch (Exception e) {
+			System.out.print("Error loading Mysql Driver!");
+			e.printStackTrace();
+		}
+		try {
+			Connection connect = DriverManager.getConnection("jdbc:mysql://192.168.1.104:3306/HelloMySQL", "root", "159753");
+
+			System.out.println("Success connect MySql server!");
+			Statement stmt = connect.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from user");
+			while (rs.next()) {
+				System.out.println(rs.getString("username"));
+				System.out.println(rs.getString("password"));
+			}
+		} catch (Exception e) {
+			System.out.print("get data error!");
+			e.printStackTrace();
+		}
+	}
+}
